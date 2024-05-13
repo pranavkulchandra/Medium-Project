7cb77cbf4af5da63474b378343654eec49e3d861
pranavkulchandra <109320936+pranavkulchandra@users.noreply.github.com>
2024-05-13 14:39:14 -0700

    updated Signup form

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,6 +1,18 @@
+import { SignupInput } from "@pkulchandra/medium-common-package"
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import { LabelledInput } from "./LabelledInput"
+import { EyeIcon, EyeSlash } from "./Icons"
 
 export const Auth =({type}: {type: "singup" | "signin"}) => { 
+
+    const [postInputs, setPostInputs ]  = useState<SignupInput>({
+        email : "", 
+        password : "", 
+        name : ""
+    })
+    const [ showPassword, setShowPassword] = useState(false)
+
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
             <div>
@@ -11,7 +23,32 @@ export const Auth =({type}: {type: "singup" | "signin"}) => {
                     Already Have an Account? 
                     <Link to={"/signup"} className="pl-2 underline">Login</Link> 
                 </div>
-            
+                    <LabelledInput type="text" label="Name" placeholder="John" onChange={(e) => { 
+                        setPostInputs(c => ({
+                            ...c, 
+                            name : e.target.value
+                        }))
+                    }} />
+                    <LabelledInput type="text" label="Email" placeholder="Johm@gmail.com" onChange={(e) => { 
+                        setPostInputs(c => ({
+                            ...c, 
+                            email : e.target.value
+                        }))
+                    }}/>
+                    <div className="relative">
+                    <LabelledInput type={showPassword ? "text" : "password"} label="Password" placeholder="*****" onChange={(e) => { 
+                        setPostInputs(c => ({ 
+                            ...c, 
+                            password : e.target.value
+                        }))                    
+                    }} />
+                    <button 
+                        onClick={() => setShowPassword(!showPassword)}
+                        className="absolute inset-y-0 right-0 pr-3 pb-5 flex items-center"
+                    >
+                     {showPassword ? <EyeIcon /> : <EyeSlash />}       
+                    </button>
+                    </div>
             </div>
         </div>
     </div>
